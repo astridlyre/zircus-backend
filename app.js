0959a9218d69fd4c853f2408767253795ba77c58
Erin Burton <eb@flat9.dev>
2021-07-12 21:28:54 -0700

    zircus $(now)

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
-const express = require("express")
+const express = require('express')
 const app = express()
-const cors = require("cors")
-const mongoose = require("mongoose")
-const config = require("./utils/config")
-const logger = require("./utils/logger")
-const middleware = require("./utils/middleware")
-require("express-async-errors")
+const cors = require('cors')
+const mongoose = require('mongoose')
+const config = require('./utils/config')
+const logger = require('./utils/logger')
+const middleware = require('./utils/middleware')
+require('express-async-errors')
 
 // Routers
-const invRouter = require("./controllers/inv")
+const invRouter = require('./controllers/inv')
 const loginRouter = require('./controllers/login')
 const ordersRouter = require('./controllers/orders')
 const tagLinesRouter = require('./controllers/tagline')
@@ -16,22 +16,23 @@ const { countriesRouter } = require('./controllers/countries')
 const adminRouter = require('./controllers/admin')
 
 // Start logging and connect to Database
-logger.info("connecting to ", config.MONGODB_URI)
-const connect = async () => await mongoose
-    .connect(config.MONGODB_URI, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-    })
-    .then(() => {
-        logger.info("connected to Database")
-    })
-    .catch((e) => logger.error("error connecting to Database: ", e.message))
+logger.info('connecting to ', config.MONGODB_URI)
+const connect = async () =>
+    await mongoose
+        .connect(config.MONGODB_URI, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        })
+        .then(() => {
+            logger.info('connected to Database')
+        })
+        .catch(e => logger.error('error connecting to Database: ', e.message))
 connect()
 
 // Use cors and json
 app.use(cors())
 app.use(express.json())
-app.use(express.static('build'))
+app.use(express.static('public'))
 
 // Use custom middleware to extra jwt and log reqs
 app.use(middleware.tokenExtractor)
--- controllers/admin.js
+++ controllers/admin.js
@@ -1,25 +1,14 @@
-const bcrypt = require("bcrypt")
-const AdminBro = require("admin-bro")
-const AdminBroExpress = require("@admin-bro/express")
-const AdminBroMongoose = require("@admin-bro/mongoose")
-const Underwear = require("../models/underwear")
-const Order = require("../models/order")
-const User = require("../models/user")
-AdminBro.registerAdapter(AdminBroMongoose)
-const adminBro = new AdminBro({
-    resources: [Underwear, Order, User],
-})
+const bcrypt = require('bcrypt')
+const { hasValidToken } = require('../utils/middleware')
+const adminRouter = require('express').Router()
+const Underwear = require('../models/underwear')
+const Order = require('../models/order')
+const User = require('../models/user')
 
-const adminRouter = AdminBroExpress.buildAuthenticatedRouter(adminBro, {
-    authenticate: async (username, password) => {
-        const user = await User.findOne({ username })
-        if (user) {
-            const matched = await bcrypt.compare(password, user.passwordHash)
-            if (matched) return user
-        }
-        return false
-    },
-    cookiePassword: process.env.SECRET,
+adminRouter.get('/', async (req, res) => {
+    if (!hasValidToken) res.redirect('/login')
+
+    res.sendFile('index.html', { root: 'admin' })
 })
 
 module.exports = adminRouter
