4ecf90ee33b426217cca88820985e4167cc2abd7
Erin Burton <eb@flat9.dev>
2021-07-21 14:10:11 -0700

    zircus

--- utils/config.js
+++ utils/config.js
@@ -6,7 +6,7 @@ const STRIPE_PUBLIC = process.env.STRIPE_PUBLIC
 const STRIPE_SECRET = process.env.STRIPE_SECRET
 const MAIL_USERNAME = process.env.MAIL_USERNAME
 const MAIL_PASSWORD = process.env.MAIL_PASSWORD
-const MAIL_ID = process.env.MAIL_CLIENTID
+const MAIL_ID = process.env.OAUTH_CLIENTID
 const MAIL_SECRET = process.env.OAUTH_CLIENT_SECRET
 const MAIL_REFRESH_TOKEN = process.env.OAUTH_REFRESH_TOKEN
 
